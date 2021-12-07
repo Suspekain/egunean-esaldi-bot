20ea16aa800af3a1f845baba7947effac8963a25
Ekain Susperregi <suspekain@gmail.com>
2021-12-07 22:05:13 +0100

    proctfile

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 const Twit = require('twit')
-
-const T = new Twit({
-  consumer_key:         'gH8c6mEqJFWYtrsSX4kNIL5FI',
-  consumer_secret:      'Izx7gHyWOgc2cDJvhgKRQkDQyv525uUiWupeMoJcdmLWKVzudv',
-  access_token:         '1466771736054149120-MW6LGaD9XveBbFM8zkXZzfuKeKFgHQ',
-  access_token_secret:  'WBIFCbSetSocSkRSYPiLP2vsrUi29gxru0s5vinTlL5KJ',
-  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-  strictSSL:            true,     // optional - requires SSL certificates to be valid.
-})
+const config = require('./config/config') //imports keys
 
 // Retorna un número aleatorio entre min (incluido) y max (excluido)
 function getRandomInt(min, max) {
@@ -17,7 +9,6 @@ function getRandomInt(min, max) {
 'use strict';
 
 const fs = require('fs')
-const config = require('./esalditegia.json')
 
 fs.readFile('./esalditegia.json', 'utf8', (err, esalditegiaJSON) => {
     if (err) {
@@ -30,13 +21,14 @@ fs.readFile('./esalditegia.json', 'utf8', (err, esalditegiaJSON) => {
     let gaurkoEsaldia = esalditegia.esaldiak[ausa]
 
     if (gaurkoEsaldia.Erabilgarri) {
-      gaurkoEsaldia.Erabilgarri = false
-      console.log(gaurkoEsaldia.Esaldia);
+      let T = new Twit(config)
       T.post('statuses/update', { status: gaurkoEsaldia.Esaldia }, function(err, data, response) {
+        gaurkoEsaldia.Erabilgarri = false
         console.log(data.text)
       })
     } else {
       console.log("errepikatua");
+      //beste saiakera vat
     }
     console.log(esalditegia.esaldiak[0].Erabilgarri)
 
